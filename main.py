8ccc7d529ca10fc7102251183bd5b839c425a4af
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-03-25 11:25:59 +0300

    create func get_coords for the city

---
+++ main.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def get_coordinates(city_name):
+    try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            'geocode': city_name,
+            'format': 'json'
+        }
+
+        response = requests.get(url, params)
+        json = response.json()
+        coordinates_str = json['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['Point']['pos']
+        long, lat = map(float, coordinates_str.split())
+        return long, lat
+
+    except Exception as e:
+        return e
