247b3376857a25ae6e88f1ddf9d852c3ca8f2a6c
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-03-25 11:26:53 +0300

    create func get_country for the city

--- main.py
+++ main.py
@@ -19,3 +19,21 @@ def get_coordinates(city_name):
 
     except Exception as e:
         return e
+
+
+def get_country(city_name):
+    try:
+        url = "https://geocode-maps.yandex.ru/1.x/"
+        params = {
+            "apikey": "40d1649f-0493-4b70-98ba-98533de7710b",
+            'geocode': city_name,
+            'format': 'json'
+        }
+
+        data = requests.get(url, params).json()
+        return data['response']['GeoObjectCollection'][
+            'featureMember'][0]['GeoObject']['metaDataProperty'][
+            'GeocoderMetaData']['AddressDetails']['Country']['CountryName']
+
+    except Exception as e:
+        return e
