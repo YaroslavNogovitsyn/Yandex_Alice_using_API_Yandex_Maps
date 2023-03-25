8d6d4bde2702d3f01b9e13330d8463e789070e8d
Yaroslav <yaroslav.nogovitsyn@yandex.ru>
2023-03-25 11:27:29 +0300

    create func get_distance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+import math
 
 
 def get_coordinates(city_name):
@@ -37,3 +38,21 @@ def get_country(city_name):
 
     except Exception as e:
         return e
+
+
+def get_distance(p1, p2):
+    radius = 6373.0
+
+    lon1 = math.radians(p1[0])
+    lat1 = math.radians(p1[1])
+    lon2 = math.radians(p2[0])
+    lat2 = math.radians(p2[1])
+
+    d_lon = lon2 - lon1
+    d_lat = lat2 - lat1
+
+    a = math.sin(d_lat / 2) ** 2 + math.cos(lat1) * math.cos(lat2) * math.sin(d_lon / 2) ** 2
+    c = 2 * math.atan2(a ** 0.5, (1 - a) ** 0.5)
+
+    distance = radius * c
+    return distance
